722bef1db83701f0774478a6dab60955899fc42a
Ankush-25 <repppol.me@gmail.com>
2025-07-04 22:08:42 +0530

    changes

--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -1,5 +1,114 @@
-import React from 'react';
-function SignUp(){
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth } from "../../authContext";
+import { Link } from "react-router-dom";
+import { PageHeader, Box, Button } from "@primer/react";
+import logo from "../../assets/github-mark-white.png";
+import "./auth.css";
+
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setloading] = useState(false);
+  const { setCurrentUser } = useAuth();
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+        setloading(true);
+        const res = await axios.post("http://localhost:3000/signup/",{
+            email:email,
+            password:password,
+            username:username
+        });
+            localStorage.setItem("token", res.data.token);
+            localStorage.setItem("userId", res.data.userId);
+            setCurrentUser(res.data.userId);
+            setloading(false);
+            window.location.href ="/";
+            console.log("SignUp Successfully done")
+    } catch (error) {
+        console.error(error);
+        alert("signup failed");
+    }
+
+  };
+
+  return (
+    <div className="login-wrapper">
+      <div className="login-logo-container">
+        <img className="logo-login" src={logo} alt="Logo" />
+      </div>
+
+      <div className="login-box-wrapper">
+        <div className="login-heading">
+          <Box sx={{ padding: 1 }}>
+            <PageHeader>
+              <PageHeader.TitleArea variant="large">
+                <PageHeader.Title>Sign Up</PageHeader.Title>
+              </PageHeader.TitleArea>
+            </PageHeader>
+          </Box>
+        </div>
+
+        <div className="login-box">
+          <div>
+            <label className="label">Username</label>
+            <input
+              autoComplete="off"
+              name="Username"
+              id="Username"
+              className="input"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label className="label">Email address</label>
+            <input
+              autoComplete="off"
+              name="Email"
+              id="Email"
+              className="input"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="div">
+            <label className="label">Password</label>
+            <input
+              autoComplete="off"
+              name="Password"
+              id="Password"
+              className="input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <Button
+            variant="primary"
+            className="login-btn"
+            disabled={loading}
+            onClick={handleSignup}
+          >
+            {loading ? "Loading..." : "Signup"}
+          </Button>
+        </div>
+
+        <div className="pass-box">
+          <p>
+            Already have an account? <Link to="/auth">Login</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import ProjectRoutes from "./Routes.jsx";
+import { AuthProvider } from "./authContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <Router>
+        <ProjectRoutes />
+      </Router>
+    </AuthProvider>
+  </StrictMode>
+);
